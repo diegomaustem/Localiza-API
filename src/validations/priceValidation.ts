f9438dcb424ee9ac4b685290dfe63b93d2863a4e
diegomaustem <diego.maustem@gmail.com>
2025-07-16 14:02:48 -0300

    :sparkles: feat: Creating validations of prices

---
+++ src/validations/priceValidation.ts
@@ -0,0 +1,14 @@
+import Joi from "joi";
+
+export const priceSchema = Joi.object({
+  price: Joi.number().precision(2).min(0).required().messages({
+    "number.base": "Price must be a number.",
+    "number.precision": "Price can have at most {#limit} decimal places.",
+    "number.min": "Price cannot be a negative value.",
+    "any.required": "Price is required.",
+  }),
+});
+
+export const updatePriceSchema = Joi.object({
+  name: Joi.number().precision(2).min(3).required(),
+});
