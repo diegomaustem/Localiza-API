869d0dc4ae3d24f900f17145c6314f75e4e6c9d1
diegomaustem <diego.maustem@gmail.com>
2025-07-17 16:10:27 -0300

    :sparkles: feat: Creating repository unit

---
+++ src/repositories/UnitRepository.ts
@@ -0,0 +1,37 @@
+import { IUnit } from "../interfaces/IUnit";
+import prisma from "../lib/prisma";
+
+class UnitRepository {
+  async findMany(): Promise<IUnit[]> {
+    try {
+      return await prisma.units.findMany();
+    } catch (error) {
+      console.error("Error fetching units.", error);
+      throw error;
+    }
+  }
+
+  async findOne(unitId: string): Promise<IUnit | null> {
+    try {
+      return await prisma.units.findUnique({
+        where: { id: unitId },
+      });
+    } catch (error) {
+      console.error("Error fetching unit.", error);
+      throw error;
+    }
+  }
+
+  async create(unit: IUnit): Promise<IUnit> {
+    try {
+      return await prisma.units.create({
+        data: unit,
+      });
+    } catch (error) {
+      console.error("Error fetching unit.", error);
+      throw error;
+    }
+  }
+}
+
+export const unitRepository = new UnitRepository();
