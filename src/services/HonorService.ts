93c4bd8fcf281a742bd782700f933cb4c5bc362c
diegomaustem <diego.maustem@gmail.com>
2025-07-19 13:17:27 -0300

    :wastebasket: remove: Remove unused methods of honor service

--- src/services/HonorService.ts
+++ src/services/HonorService.ts
@@ -1,8 +1,5 @@
 import { IHonor } from "../interfaces/IHonor";
-import { genericRepository } from "../repositories/GenericRepository";
 import { honorRepository } from "../repositories/HonorRepository";
-import { ValidPrismaTable } from "../types/PrismaTables";
-
 class HonorService {
   async getHonors(): Promise<IHonor[]> {
     try {
@@ -20,41 +17,6 @@ class HonorService {
       console.error("Failed to retrieve honor.", error);
       throw error;
     }
-  }
-
-  async createHonor(honor: IHonor): Promise<IHonor> {
-    try {
-      return await honorRepository.create(honor);
-    } catch (error) {
-      console.error("Failed to create honor.", error);
-      throw error;
-    }
-  }
-
-  async updateHonor(honorId: string, honorData: IHonor): Promise<IHonor> {
-    try {
-      return await honorRepository.update(honorId, honorData);
-    } catch (error) {
-      console.error("Failed to update honor.", error);
-      throw error;
-    }
-  }
-
-  async deleteHonor(honorId: string): Promise<IHonor> {
-    try {
-      return await honorRepository.delete(honorId);
-    } catch (error) {
-      console.error("Failed to delete honor.", error);
-      throw error;
-    }
-  }
-
-  async verifyCustomerHonor(honorId: string): Promise<boolean> {
-    const table: ValidPrismaTable = "customers";
-    const field = "honors_id";
-    const value = honorId;
-
-    return genericRepository.generateQuery(table, field, value);
   }
 }
 
