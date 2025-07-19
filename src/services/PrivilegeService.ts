40c7a33cfc16f984be92c08cb82b051717f30ac7
diegomaustem <diego.maustem@gmail.com>
2025-07-19 11:52:34 -0300

    :wastebasket: remove: Remove unused methods of service privilege

--- src/services/PrivilegeService.ts
+++ src/services/PrivilegeService.ts
@@ -21,52 +21,6 @@ class PrivilegeService {
       throw error;
     }
   }
-
-  async createPrivigele(privilegeData: IPrivilege): Promise<IPrivilege> {
-    try {
-      return await privilegeRepository.create(privilegeData);
-    } catch (error) {
-      console.error("Failed to create privilege.", error);
-      throw error;
-    }
-  }
-
-  async updatePrivilege(
-    privilegeId: string,
-    privilegeData: IPrivilege
-  ): Promise<IPrivilege> {
-    try {
-      return await privilegeRepository.update(privilegeId, privilegeData);
-    } catch (error) {
-      console.error("Failed to update privilege.", error);
-      throw error;
-    }
-  }
-
-  async deletePrivilege(privilegeId: string): Promise<IPrivilege> {
-    try {
-      return await privilegeRepository.delete(privilegeId);
-    } catch (error) {
-      console.error("Failed to delete privilege.", error);
-      throw error;
-    }
-  }
-
-  async verifyPrivilege(privilegeData: IPrivilege): Promise<boolean> {
-    const table: ValidPrismaTable = "privileges";
-    const field = "name";
-    const value = privilegeData.name;
-
-    return genericRepository.generateQuery(table, field, value);
-  }
-
-  async verifyUserPrivilege(privilegeId: string): Promise<boolean> {
-    const table: ValidPrismaTable = "users";
-    const field = "privileges_id";
-    const value = privilegeId;
-
-    return genericRepository.generateQuery(table, field, value);
-  }
 }
 
 export const privilegeService = new PrivilegeService();
