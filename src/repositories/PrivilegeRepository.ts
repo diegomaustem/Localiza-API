1237387d1d9140e59023e6b36b41a2c1d389d458
diegomaustem <diego.maustem@gmail.com>
2025-07-19 11:51:10 -0300

    :wastebasket: remove: Remove unused methods of repository privilege

--- src/repositories/PrivilegeRepository.ts
+++ src/repositories/PrivilegeRepository.ts
@@ -21,43 +21,6 @@ class PrivilegeRepository {
       throw error;
     }
   }
-
-  async create(privilegeData: IPrivilege): Promise<IPrivilege> {
-    try {
-      return await prisma.privileges.create({
-        data: privilegeData,
-      });
-    } catch (error) {
-      console.error("Error fetching privileges.", error);
-      throw error;
-    }
-  }
-
-  async update(
-    privilegeId: string,
-    privilegeData: IPrivilege
-  ): Promise<IPrivilege> {
-    try {
-      return await prisma.privileges.update({
-        where: { id: privilegeId },
-        data: privilegeData,
-      });
-    } catch (error) {
-      console.error("Error updating privilege.", error);
-      throw error;
-    }
-  }
-
-  async delete(privilegeId: string): Promise<IPrivilege> {
-    try {
-      return await prisma.privileges.delete({
-        where: { id: privilegeId },
-      });
-    } catch (error) {
-      console.error("Error deleting privilege.", error);
-      throw error;
-    }
-  }
 }
 
 export const privilegeRepository = new PrivilegeRepository();
