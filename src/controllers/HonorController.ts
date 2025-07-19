a0ceeab3dd90ac11a608504287f67532c04a8712
diegomaustem <diego.maustem@gmail.com>
2025-07-19 13:18:14 -0300

    :wastebasket: remove: Remove unused methods of honor controller

--- src/controllers/HonorController.ts
+++ src/controllers/HonorController.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import { honorService } from "../services/HonorService";
-import { IHonor } from "../interfaces/IHonor";
 class HonorController {
   async getHonors(req: Request, res: Response): Promise<void> {
     try {
@@ -42,103 +41,6 @@ class HonorController {
         code: 500,
         status: "error",
         message: "Internal error while searching for honor.",
-      });
-    }
-  }
-
-  async createHonor(req: Request, res: Response): Promise<void> {
-    const honorData: IHonor = req.body;
-
-    try {
-      const createdHonor = await honorService.createHonor(honorData);
-
-      res.status(201).json({
-        code: 201,
-        status: "success",
-        message: "Honor created successfully.",
-        createdHonor: createdHonor,
-      });
-    } catch (error) {
-      console.error("Error creating honor.", error);
-      res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Internal error while creating honor.",
-      });
-    }
-  }
-
-  async updateHonor(req: Request, res: Response): Promise<void> {
-    const honorId: string = req.params.id;
-    const honorData: IHonor = req.body;
-
-    try {
-      const honor = await honorService.getHonor(honorId);
-      if (!honor) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "Honor not found for update.",
-        });
-        return;
-      }
-
-      const updatedHonor = await honorService.updateHonor(honorId, honorData);
-
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        message: "Honor updated successfully.",
-        updatedHonor: updatedHonor,
-      });
-    } catch (error) {
-      console.error("Error updating honor.", error);
-      res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Internal error while updating honor.",
-      });
-    }
-  }
-
-  async deleteHonor(req: Request, res: Response): Promise<void> {
-    const honorId: string = req.params.id;
-
-    try {
-      const honor = await honorService.getHonor(honorId);
-
-      if (!honor) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "Honor not found for deletion.",
-        });
-        return;
-      }
-
-      const hasCustomerHonor = await honorService.verifyCustomerHonor(honorId);
-      if (hasCustomerHonor) {
-        res.status(409).json({
-          code: 409,
-          status: "error",
-          message: "The honor is in use. It cannot be deleted.",
-        });
-        return;
-      }
-
-      const deletedHonor = await honorService.deleteHonor(honorId);
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        message: "Honor deleted successfully.",
-        deletedHonor: deletedHonor,
-      });
-    } catch (error) {
-      console.error("Error deleting honor.", error);
-      res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Internal error while deleting honor.",
       });
     }
   }
