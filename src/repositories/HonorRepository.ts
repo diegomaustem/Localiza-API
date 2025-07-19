5dd76f6c464e50d340acd1487038c80b4b231907
diegomaustem <diego.maustem@gmail.com>
2025-07-19 13:16:43 -0300

    :wastebasket: remove: Remove unused methods of honor repository

--- src/repositories/HonorRepository.ts
+++ src/repositories/HonorRepository.ts
@@ -21,40 +21,6 @@ class HonorRepository {
       throw error;
     }
   }
-
-  async create(honor: IHonor): Promise<IHonor> {
-    try {
-      return await prisma.honors.create({
-        data: honor,
-      });
-    } catch (error) {
-      console.error("Error fetching honor.", error);
-      throw error;
-    }
-  }
-
-  async update(honorId: string, honorData: IHonor): Promise<IHonor> {
-    try {
-      return await prisma.honors.update({
-        where: { id: honorId },
-        data: honorData,
-      });
-    } catch (error) {
-      console.error("Error updating honor.", error);
-      throw error;
-    }
-  }
-
-  async delete(honorId: string): Promise<IHonor> {
-    try {
-      return await prisma.honors.delete({
-        where: { id: honorId },
-      });
-    } catch (error) {
-      console.error("Error deleting honor.", error);
-      throw error;
-    }
-  }
 }
 
 export const honorRepository = new HonorRepository();
