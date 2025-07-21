9172e9837d47ac9004ee3788f52559de516ef830
diegomaustem <diego.maustem@gmail.com>
2025-07-21 19:09:00 -0300

    :recycle: refactor: Adjustments methods of controller units

--- src/controllers/UnitController.ts
+++ src/controllers/UnitController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { IUnit } from "../interfaces/IUnit";
 import { unitService } from "../services/UnitService";
 import HttpError from "../errors/HttpError";
-
 class UnitController {
   async getUnits(req: Request, res: Response): Promise<void> {
     try {
@@ -59,7 +58,7 @@ class UnitController {
       res.status(201).json({
         code: 201,
         status: "success",
-        message: "Unit created successfully.",
+        message: "Unidade criada com sucesso.",
         createdUnit: createdUnit,
       });
     } catch (error) {
@@ -76,7 +75,94 @@ class UnitController {
       res.status(500).json({
         code: 500,
         status: "error",
-        message: "Internal error while creating unit.",
+        message: "Erro interno ao criar unidade.",
+      });
+    }
+  }
+
+  async updateUnit(req: Request, res: Response): Promise<void> {
+    const unitId: string = req.params.id;
+    const unitData: IUnit = req.body;
+
+    try {
+      const unit = await unitService.getUnit(unitId);
+      if (!unit) {
+        res.status(404).json({
+          code: 404,
+          status: "error",
+          message: "Unidade não encontrada para atualização.",
+        });
+        return;
+      }
+
+      await unitService.unitRulesValidation(unitData);
+
+      const updatedUnit = await unitService.updateUnit(unitId, unitData);
+
+      res.status(200).json({
+        code: 200,
+        status: "success",
+        message: "Unidade atualizada com sucesso.",
+        updatedUnit: updatedUnit,
+      });
+    } catch (error) {
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({
+          code: error.statusCode,
+          status: "error",
+          message: error.message,
+        });
+        return;
+      }
+
+      console.error("Error updating unit.", error);
+      res.status(500).json({
+        code: 500,
+        status: "error",
+        message: "Erro interno ao atualizar unidade.",
+      });
+    }
+  }
+
+  async deleteUnit(req: Request, res: Response): Promise<void> {
+    const unitId: string = req.params.id;
+
+    try {
+      const unit = await unitService.getUnit(unitId);
+
+      if (!unit) {
+        res.status(404).json({
+          code: 404,
+          status: "error",
+          message: "Unidade não encontrada para exclusão.",
+        });
+        return;
+      }
+
+      await unitService.unitRulesValidation(undefined, unitId);
+
+      const deletedUnit = await unitService.deleteUnit(unitId);
+
+      res.status(200).json({
+        code: 200,
+        status: "success",
+        message: "Unidade excluída com sucesso.",
+        deletedUnit: deletedUnit,
+      });
+    } catch (error) {
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({
+          code: error.statusCode,
+          status: "error",
+          message: error.message,
+        });
+        return;
+      }
+      console.error("Error deleting unit.", error);
+      res.status(500).json({
+        code: 500,
+        status: "error",
+        message: "Erro interno ao excluir unidade.",
       });
     }
   }
