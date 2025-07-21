4bffcbb6d46a98010d92cd3869197b1d51d94d16
diegomaustem <diego.maustem@gmail.com>
2025-07-21 12:55:13 -0300

    :recycle: refactor: Adjustment controller of states

--- src/controllers/StateController.ts
+++ src/controllers/StateController.ts
@@ -1,8 +1,5 @@
-import { IState } from "../interfaces/IState";
 import { Request, Response } from "express";
 import { stateService } from "../services/StateService";
-import HttpError from "../errors/HttpError";
-
 class StateController {
   async getStates(req: Request, res: Response): Promise<void> {
     try {
@@ -14,7 +11,7 @@ class StateController {
       res.status(500).json({
         code: 500,
         status: "error",
-        message: "Internal error while searching for states.",
+        message: "Erro interno ao buscar estados.",
       });
     }
   }
@@ -28,7 +25,7 @@ class StateController {
         res.status(404).json({
           code: 404,
           status: "error",
-          message: "State not found.",
+          message: "Estado não encontrado.",
         });
         return;
       }
@@ -43,40 +40,7 @@ class StateController {
       res.status(500).json({
         code: 500,
         status: "error",
-        message: "Internal error while searching for state.",
-      });
-    }
-  }
-
-  async createState(req: Request, res: Response): Promise<void> {
-    const stateData: IState = req.body;
-
-    try {
-      await stateService.stateRulesValidation(stateData);
-
-      const createdState = await stateService.createState(stateData);
-
-      res.status(201).json({
-        code: 201,
-        status: "success",
-        message: "State created successfully.",
-        createdState: createdState,
-      });
-    } catch (error) {
-      if (error instanceof HttpError) {
-        res.status(error.statusCode).json({
-          code: error.statusCode,
-          status: "error",
-          message: error.message,
-        });
-        return;
-      }
-
-      console.error("Error creating state.", error);
-      res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Internal error while creating state.",
+        message: "Erro interno ao buscar estado.",
       });
     }
   }
