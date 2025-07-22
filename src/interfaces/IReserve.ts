9e9b64f549725d4b1bfb548279e02c68d029b373
diegomaustem <diego.maustem@gmail.com>
2025-07-22 14:35:01 -0300

    :recycle: refactor: Adjustments reserve interface

--- src/interfaces/IReserve.ts
+++ src/interfaces/IReserve.ts
@@ -1,9 +1,10 @@
+import { Decimal } from "../generated/prisma/runtime/library";
 export interface IReserve {
   id: string;
   daily_amount: number;
   withdrawn: Date;
   return: Date;
-  value: number;
+  value: Decimal;
   status: string;
   customers_id: string;
   vehicles_id: string;
