d1f0914e8a7b8407f555dd366893e1e995db8715
diegomaustem <diego.maustem@gmail.com>
2025-07-22 14:31:37 -0300

    :recycle: refactor: Adjustments methods update and delete of vehicle repository

--- src/repositories/ReserveRepository.ts
+++ src/repositories/ReserveRepository.ts
@@ -32,6 +32,29 @@ class ReserveRepository {
       throw error;
     }
   }
+
+  async update(reserveId: string, reserveData: IReserve): Promise<IReserve> {
+    try {
+      return await prisma.reserves.update({
+        where: { id: reserveId },
+        data: reserveData,
+      });
+    } catch (error) {
+      console.error("Error updating reserve.", error);
+      throw error;
+    }
+  }
+
+  async delete(reserveId: string): Promise<IReserve> {
+    try {
+      return await prisma.reserves.delete({
+        where: { id: reserveId },
+      });
+    } catch (error) {
+      console.error("Error deleting reserve.", error);
+      throw error;
+    }
+  }
 }
 
 export const reserveRepository = new ReserveRepository();
