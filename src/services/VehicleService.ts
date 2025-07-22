3c3526d9d4c5e8259602f4863cb3f19f3a9ef73c
diegomaustem <diego.maustem@gmail.com>
2025-07-22 14:14:31 -0300

    :recycle: refactor: Adjustments service of vehicle

--- src/services/VehicleService.ts
+++ src/services/VehicleService.ts
@@ -1,6 +1,8 @@
-import { ICreateVehicle, IVehicle } from "../interfaces/IVehicle";
+import { IVehicle } from "../interfaces/IVehicle";
 import { v4 as uuidv4 } from "uuid";
 import VehicleRepository from "../repositories/VehicleRepository";
+import { genericRepository } from "../repositories/GenericRepository";
+import HttpError from "../errors/HttpError";
 
 class VehicleService {
   async getVehicles(): Promise<IVehicle[]> {
@@ -21,8 +23,8 @@ class VehicleService {
     }
   }
 
-  async createVehicle(vehicle: ICreateVehicle): Promise<IVehicle> {
-    const newVehicle: ICreateVehicle = {
+  async createVehicle(vehicle: IVehicle): Promise<IVehicle> {
+    const newVehicle: IVehicle = {
       ...vehicle,
       id: uuidv4(),
     };
@@ -37,7 +39,7 @@ class VehicleService {
 
   async updateVehicle(
     vehicleId: string,
-    vehicleData: ICreateVehicle
+    vehicleData: IVehicle
   ): Promise<IVehicle> {
     try {
       return await VehicleRepository.update(vehicleId, vehicleData);
@@ -53,6 +55,111 @@ class VehicleService {
     } catch (error) {
       console.error("Failed to delete vehicle.", error);
       throw error;
+    }
+  }
+
+  async vehicleRulesValidation(
+    vehicleData?: IVehicle,
+    vehicleId?: string
+  ): Promise<void> {
+    if (vehicleId) {
+      const isLinked = await genericRepository.generateQuery(
+        "reserves",
+        "vehicles_id",
+        vehicleId
+      );
+      if (isLinked) {
+        throw new HttpError(
+          "Este veículo está vinculado a uma ou mais reservas e não pode ser excluído.",
+          409
+        );
+      }
+      return;
+    }
+
+    if (vehicleData) {
+      const {
+        chassi,
+        plate,
+        crlv_document,
+        groups_id,
+        units_id,
+        categories_id,
+      } = vehicleData;
+
+      const [
+        existingChassi,
+        existingPlate,
+        existingDocument,
+        validGroup,
+        validUnit,
+        validCategory,
+      ] = await Promise.all([
+        chassi
+          ? genericRepository.generateQuery("vehicles", "chassi", chassi)
+          : null,
+        plate
+          ? genericRepository.generateQuery("vehicles", "plate", plate)
+          : null,
+        crlv_document
+          ? genericRepository.generateQuery(
+              "vehicles",
+              "crlv_document",
+              crlv_document
+            )
+          : null,
+        groups_id
+          ? genericRepository.generateQuery("groups", "id", groups_id)
+          : null,
+        units_id
+          ? genericRepository.generateQuery("units", "id", units_id)
+          : null,
+        categories_id
+          ? genericRepository.generateQuery("categories", "id", categories_id)
+          : null,
+      ]);
+
+      if (existingChassi) {
+        throw new HttpError(
+          "Já existe um veículo registrado com este chassi. Tente outro valor.",
+          409
+        );
+      }
+
+      if (existingPlate) {
+        throw new HttpError(
+          "Já existe um veículo registrado com esta placa. Tente outro valor.",
+          409
+        );
+      }
+
+      if (existingDocument) {
+        throw new HttpError(
+          "O documento informado já existe em nossos registros. Tente outro valor.",
+          409
+        );
+      }
+
+      if (groups_id && !validGroup) {
+        throw new HttpError(
+          "O grupo informado não consta em nossos registros. Tente outro.",
+          404
+        );
+      }
+
+      if (units_id && !validUnit) {
+        throw new HttpError(
+          "A unidade informada não consta em nossos registros. Tente outra.",
+          404
+        );
+      }
+
+      if (categories_id && !validCategory) {
+        throw new HttpError(
+          "A categoria informada não consta em nossos registros. Tente outra.",
+          404
+        );
+      }
     }
   }
 }
