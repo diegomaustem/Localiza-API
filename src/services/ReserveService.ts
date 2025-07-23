f5eb2ecd178cc84d779d7801f780ccdcc00df42f
diegomaustem <diego.maustem@gmail.com>
2025-07-23 10:37:34 -0300

    :recycle: refactor: Adjustments create update and delete methods of reserves service

--- src/services/ReserveService.ts
+++ src/services/ReserveService.ts
@@ -1,6 +1,7 @@
+import HttpError from "../errors/HttpError";
 import { IReserve } from "../interfaces/IReserve";
+import { genericRepository } from "../repositories/GenericRepository";
 import { reserveRepository } from "../repositories/ReserveRepository";
-
 class ReserveService {
   async getReserves(): Promise<IReserve[]> {
     try {
@@ -26,6 +27,74 @@ class ReserveService {
     } catch (error) {
       console.error("Failed to create reserve.", error);
       throw error;
+    }
+  }
+
+  async updateReserve(
+    reserveData: IReserve,
+    reserveId: string
+  ): Promise<IReserve> {
+    try {
+      return await reserveRepository.update(reserveId, reserveData);
+    } catch (error) {
+      console.error("Failed to update reserve.", error);
+      throw error;
+    }
+  }
+
+  async deleteReserve(reserveId: string): Promise<IReserve> {
+    try {
+      return await reserveRepository.delete(reserveId);
+    } catch (error) {
+      console.error("Failed to delete reserve.", error);
+      throw error;
+    }
+  }
+
+  async reserveRulesValidation(
+    reserveData?: IReserve,
+    reserveId?: string
+  ): Promise<void> {
+    if (reserveData) {
+      const { customers_id, vehicles_id } = reserveData;
+
+      const [customerExists, vehicleExists, availableVehicle] =
+        await Promise.all([
+          customers_id
+            ? genericRepository.generateQuery("customers", "id", customers_id)
+            : Promise.resolve(false),
+          vehicles_id
+            ? genericRepository.generateQuery("vehicles", "id", vehicles_id)
+            : Promise.resolve(false),
+          vehicles_id
+            ? genericRepository.generateQuery(
+                "reserves",
+                "vehicles_id",
+                vehicles_id
+              )
+            : Promise.resolve(false),
+        ]);
+
+      if (customers_id && !customerExists) {
+        throw new HttpError(
+          "Cliente não encontrado. Insira um cliente válido.",
+          404
+        );
+      }
+
+      if (vehicles_id && !vehicleExists) {
+        throw new HttpError(
+          "Veículo não encontrado. Insira um veículo válido.",
+          404
+        );
+      }
+
+      if (vehicles_id && availableVehicle) {
+        throw new HttpError(
+          "Veículo indisponível para locação. Tente outro veículo.",
+          404
+        );
+      }
     }
   }
 }
