a9670ec20715d50df37db745167bef4fdf8e8d40
diegomaustem <diego.maustem@gmail.com>
2025-07-23 11:01:20 -0300

    :wastebasket: remove: Remove spaces in code nationality controller

--- src/controllers/NationalityController.ts
+++ src/controllers/NationalityController.ts
@@ -2,7 +2,6 @@ import { Request, Response } from "express";
 import { nationalityService } from "../services/NationalityService";
 import { INationality } from "../interfaces/INationality";
 import HttpError from "../errors/HttpError";
-
 class NationalityController {
   async getNationalities(req: Request, res: Response): Promise<void> {
     try {
