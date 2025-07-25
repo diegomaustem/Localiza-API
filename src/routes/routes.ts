3d55515269e00cbaaa313ed8a7c72e08ff42370a
diegomaustem <diego.maustem@gmail.com>
2025-07-25 11:16:27 -0300

    :sparkles: feat: Creating login router and implementing auth in routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,7 @@ import { vehicleController } from "../controllers/VehicleController";
 import { privilegeController } from "../controllers/PrivilegeController";
 import { nationalityController } from "../controllers/NationalityController";
 import { validate } from "../middlewares/validationMiddleware";
+import { auth } from "../middlewares/authMiddleware";
 import {
   createUserSchema,
   updateUserSchema,
@@ -52,128 +53,162 @@ const router = Router();
 router.post("/login", validate(loginSchema, "body"), authController.login);
 
 // Users :::
-router.get("/users", userController.getUsers);
-router.get("/user/:id", userController.getUser);
+router.get("/users", auth, userController.getUsers);
+router.get("/user/:id", auth, userController.getUser);
 router.post(
   "/user",
   validate(createUserSchema, "body"),
+  auth,
   userController.createUser
 );
 router.patch(
   "/user/:id",
   validate(updateUserSchema, "body"),
+  auth,
   userController.updateUser
 );
-router.delete("/user/:id", userController.deleteUser);
+router.delete("/user/:id", auth, userController.deleteUser);
 
 // Privileges :::
-router.get("/privileges", privilegeController.getPrivileges);
-router.get("/privilege/:id", privilegeController.getPrivilege);
+router.get("/privileges", auth, privilegeController.getPrivileges);
+router.get("/privilege/:id", auth, privilegeController.getPrivilege);
 
 // Status users :::
-router.get("/statusUsers", statusUserController.getStatusUsers);
-router.get("/statusUser/:id", statusUserController.getStatusUser);
+router.get("/statusUsers", auth, statusUserController.getStatusUsers);
+router.get("/statusUser/:id", auth, statusUserController.getStatusUser);
 
 // Status customers :::
-router.get("/statusCustomers", statusCustomerController.getStatusCustomers);
-router.get("/statusCustomer/:id", statusCustomerController.getStatusCustomer);
+router.get(
+  "/statusCustomers",
+  auth,
+  statusCustomerController.getStatusCustomers
+);
+router.get(
+  "/statusCustomer/:id",
+  auth,
+  statusCustomerController.getStatusCustomer
+);
 
 // Nationalities :::
-router.get("/nationalities", nationalityController.getNationalities);
-router.get("/nationality/:id", nationalityController.getNationality);
+router.get("/nationalities", auth, nationalityController.getNationalities);
+router.get("/nationality/:id", auth, nationalityController.getNationality);
 router.post(
   "/nationality",
   validate(createNationalitySchema),
+  auth,
   nationalityController.createNationality
 );
 router.patch(
   "/nationality/:id",
   validate(updateNationalitySchema),
+  auth,
   nationalityController.updateNationality
 );
-router.delete("/nationality/:id", nationalityController.deleteNationality);
+router.delete(
+  "/nationality/:id",
+  auth,
+  nationalityController.deleteNationality
+);
 
 // Customers :::
-router.get("/customers", customerController.getCustomers);
-router.get("/customer/:id", customerController.getCustomer);
+router.get("/customers", auth, customerController.getCustomers);
+router.get("/customer/:id", auth, customerController.getCustomer);
 
 router.post(
   "/customer",
   validate(createCustomerSchema),
+  auth,
   customerController.createCustomer
 );
 router.patch(
   "/customer/:id",
   validate(updateCustomerSchema),
+  auth,
   customerController.updateCustomer
 );
-router.delete("/customer/:id", customerController.deleteCustomer);
+router.delete("/customer/:id", auth, customerController.deleteCustomer);
 
 // Vehicles :::
-router.get("/vehicles", vehicleController.getVehicles);
-router.get("/vehicle/:id", vehicleController.getVehicle);
+router.get("/vehicles", auth, vehicleController.getVehicles);
+router.get("/vehicle/:id", auth, vehicleController.getVehicle);
 router.post(
   "/vehicle",
   validate(createVehicleSchema, "body"),
+  auth,
   vehicleController.createVehicle
 );
 router.patch(
   "/vehicle/:id",
   validate(updateVehicleSchema, "body"),
+  auth,
   vehicleController.updateVehicle
 );
-router.delete("/vehicle/:id", vehicleController.deleteVehicle);
+router.delete("/vehicle/:id", auth, vehicleController.deleteVehicle);
 
 // Honors :::
-router.get("/honors", honorController.getHonors);
-router.get("/honor/:id", honorController.getHonor);
+router.get("/honors", auth, honorController.getHonors);
+router.get("/honor/:id", auth, honorController.getHonor);
 
 // Categories :::
-router.get("/categories", categoryController.getCategories);
-router.get("/category/:id", categoryController.getCategory);
+router.get("/categories", auth, categoryController.getCategories);
+router.get("/category/:id", auth, categoryController.getCategory);
 
 // Groups :::
-router.get("/groups", groupController.getGroups);
-router.get("/group/:id", groupController.getGroup);
+router.get("/groups", auth, groupController.getGroups);
+router.get("/group/:id", auth, groupController.getGroup);
 
 // States :::
-router.get("/states", stateController.getStates);
-router.get("/state/:id", stateController.getState);
+router.get("/states", auth, stateController.getStates);
+router.get("/state/:id", auth, stateController.getState);
 
 // Cities :::
-router.get("/cities", cityController.getCities);
-router.get("/city/:id", cityController.getCity);
-router.post("/city", validate(createCitySchema), cityController.createCity);
+router.get("/cities", auth, cityController.getCities);
+router.get("/city/:id", auth, cityController.getCity);
+router.post(
+  "/city",
+  validate(createCitySchema),
+  auth,
+  cityController.createCity
+);
 router.patch(
   "/city/:id",
   validate(updateCitySchema),
+  auth,
   cityController.updateCity
 );
-router.delete("/city/:id", cityController.deleteCity);
+router.delete("/city/:id", auth, cityController.deleteCity);
 
 // Units :::
-router.get("/units", unitController.getUnits);
-router.get("/unit/:id", unitController.getUnit);
-router.post("/unit", validate(createUnitSchema), unitController.createUnit);
+router.get("/units", auth, unitController.getUnits);
+router.get("/unit/:id", auth, unitController.getUnit);
+router.post(
+  "/unit",
+  validate(createUnitSchema),
+  auth,
+  unitController.createUnit
+);
 router.patch(
   "/unit/:id",
   validate(updateUnitSchema),
+  auth,
   unitController.updateUnit
 );
-router.delete("/unit/:id", unitController.deleteUnit);
+router.delete("/unit/:id", auth, unitController.deleteUnit);
 
 // Reserves :::
-router.get("/reserves", reserveController.getReserves);
-router.get("/reserve/:id", reserveController.getReserve);
+router.get("/reserves", auth, reserveController.getReserves);
+router.get("/reserve/:id", auth, reserveController.getReserve);
 router.post(
   "/reserve",
   validate(createReserveSchema),
+  auth,
   reserveController.createReserve
 );
 router.patch(
   "/reserve/:id",
   validate(updateReserveSchema),
+  auth,
   reserveController.updateReserve
 );
-router.delete("/reserve/:id", reserveController.deleteReserve);
+router.delete("/reserve/:id", auth, reserveController.deleteReserve);
 export default router;
