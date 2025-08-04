5690772515a45391cc3f94179b2c524f41da36c3
diegomaustem <diego.maustem@gmail.com>
2025-08-04 11:28:59 -0300

    :sparkles: feat: Adding interfaces of pagination

---
+++ src/interfaces/IPaginated.ts
@@ -0,0 +1,4 @@
+export interface IPaginated {
+  page: number;
+  limit: number;
+}
---
+++ src/interfaces/IPaginatedResult.ts
@@ -0,0 +1,9 @@
+export interface IPaginatedResult<T> {
+  data: T[];
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+  };
+}
