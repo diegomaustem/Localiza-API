d4c22fd16f2884b8af237a60eb3020bace4dddb1
diegomaustem <diego.maustem@gmail.com>
2025-08-29 14:04:34 -0300

    :recycle: refactor: Adjustment of name class user

--- src/composition/users.composition.ts
+++ src/composition/users.composition.ts
@@ -2,11 +2,11 @@ import { UserController } from "../controllers/UserController";
 import prisma from "../lib/prisma";
 import { RepositoryGenericPrisma } from "../repositories/RepositoryGenericPrisma";
 import { RepositoryUserPrisma } from "../repositories/RepositoryUserPrisma";
-import { ServiceUser } from "../services/ServiceUser";
+import { UserService } from "../services/UserService";
 
 export function createUserController(): UserController {
   const repositoryUser = new RepositoryUserPrisma(prisma);
   const repositoryGeneric = new RepositoryGenericPrisma(prisma);
-  const service = new ServiceUser(repositoryUser, repositoryGeneric);
+  const service = new UserService(repositoryUser, repositoryGeneric);
   return new UserController(service);
 }
