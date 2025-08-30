e131d4845dc8dd6c1e14e7ec327af4793383977c
diegomaustem <diego.maustem@gmail.com>
2025-08-30 10:53:11 -0300

    :sparkles: feat: Create controller of statusUsers

--- src/controllers/StatusUserController.ts
+++ src/controllers/StatusUserController.ts
@@ -1,52 +1,43 @@
 import { Request, Response } from "express";
-import { statusUserService } from "../services/StatusUserService";
+import { IStatusUserService } from "../interfaces/StatusUser/IStatusUserService";
+import HttpError from "../errors/HttpError";
+export class StatusUserController {
+  constructor(private readonly service: IStatusUserService) {}
 
-class StatusUserController {
-  async getStatusUsers(req: Request, res: Response): Promise<void> {
+  listStatusUsers = async (req: Request, res: Response): Promise<void> => {
     try {
-      const statusUsers = await statusUserService.getStatusUsers();
-      res
-        .status(200)
-        .json({ code: 200, status: "success", statusUsers: statusUsers });
+      const statusUsers = await this.service.listStatusUsers();
+      res.status(200).json({ data: { statusUsers: statusUsers } });
     } catch (error) {
       console.error("Error getting statusUsers.", error);
 
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao procurar por statusUsers",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error while searching for statusUsers.",
       });
     }
-  }
+  };
 
-  async getStatusUser(req: Request, res: Response): Promise<void> {
-    const statusUserId: string = req.params.id;
-
+  listStatusUser = async (req: Request, res: Response): Promise<void> => {
+    const id: string = req.params.id;
     try {
-      const statusUser = await statusUserService.getStatusUser(statusUserId);
-      if (statusUser === null) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "statusUser não encontrado.",
+      const statusUser = await this.service.listStatusUser(id);
+      res.status(200).json({ data: { statusUsers: statusUser } });
+    } catch (error) {
+      console.error("Error getting statusUser.", error);
+
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({
+          code: error.code || "INTERNAL_SERVER_ERROR",
+          message: error.message,
         });
         return;
       }
 
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        statusUser: statusUser,
-      });
-    } catch (error) {
-      console.error("Error getting statusUser.", error);
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao buscar statusUser.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error fetching statusUser.",
       });
     }
-  }
+  };
 }
-
-export const statusUserController = new StatusUserController();
