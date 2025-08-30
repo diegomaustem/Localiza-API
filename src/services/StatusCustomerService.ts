821e8388297ebebe914358d20239dcf6e775807e
diegomaustem <diego.maustem@gmail.com>
2025-08-30 12:51:54 -0300

    :sparkles: feat: Create services of statusCustomer

--- src/services/StatusCustomerService.ts
+++ src/services/StatusCustomerService.ts
@@ -1,26 +1,34 @@
-import { IStatusCustomer } from "../interfaces/IStatusCustomer";
-import { statusCustomerRepository } from "../repositories/StatusCustomerRepository";
+import HttpError from "../errors/HttpError";
+import { IStatusCustomer } from "../interfaces/StatusCustomer/IStatusCustomer";
+import { IStatusCustomerRepository } from "../interfaces/StatusCustomer/IStatusCustomerRepository";
+import { IStatusCustomerService } from "../interfaces/StatusCustomer/IStatusCustomerService";
 
-class StatusCustomerService {
-  async getStatusCustomers(): Promise<IStatusCustomer[]> {
+export class StatusCustomerService implements IStatusCustomerService {
+  constructor(private readonly repository: IStatusCustomerRepository) {}
+
+  async listStatusCustomers(): Promise<IStatusCustomer[]> {
     try {
-      return await statusCustomerRepository.findMany();
+      return await this.repository.findMany();
     } catch (error) {
       console.error("Failed to retrieve statusCustomers.", error);
       throw error;
     }
   }
 
-  async getStatusCustomer(
-    statusCustomerId: string
-  ): Promise<IStatusCustomer | null> {
+  async listStatusCustomer(id: string): Promise<IStatusCustomer | null> {
     try {
-      return await statusCustomerRepository.findOne(statusCustomerId);
+      const statusCustomer = await this.repository.findOne(id);
+      if (!statusCustomer) {
+        throw new HttpError(
+          "RESOURCE_NOT_FOUND",
+          "StatusCustomer not found in our records.",
+          404
+        );
+      }
+      return statusCustomer;
     } catch (error) {
       console.error("Failed to retrieve statusCustomer.", error);
       throw error;
     }
   }
 }
-
-export const statusCustomerService = new StatusCustomerService();
