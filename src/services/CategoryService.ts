6c790134c5ebe6dd601899771de0b04ca76f591a
diegomaustem <diego.maustem@gmail.com>
2025-08-31 19:45:15 -0300

    :recycle: refactor

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,23 +1,34 @@
-import { ICategory } from "../interfaces/ICategory";
-import { categoryRepository } from "../repositories/CategoryRepository";
-class CategoryService {
-  async getCategories(): Promise<ICategory[]> {
+import HttpError from "../errors/HttpError";
+import { ICategory } from "../interfaces/Category/ICategory";
+import { ICategoryRepository } from "../interfaces/Category/ICategoryRepository";
+import { ICategoryService } from "../interfaces/Category/ICategoryService";
+
+export class CategoryService implements ICategoryService {
+  constructor(private readonly repository: ICategoryRepository) {}
+
+  async listCategories(): Promise<ICategory[]> {
     try {
-      return await categoryRepository.findMany();
+      return await this.repository.findMany();
     } catch (error) {
-      console.error("Failed to retrieve categories.", error);
+      console.error("[Service] - Failed to retrieve categories.", error);
       throw error;
     }
   }
 
-  async getCategory(categoryId: string): Promise<ICategory | null> {
+  async listCategory(id: string): Promise<ICategory | null> {
     try {
-      return await categoryRepository.findOne(categoryId);
+      const category = await this.repository.findOne(id);
+      if (!category) {
+        throw new HttpError(
+          "RESOURCE_NOT_FOUND",
+          "Category not found in our records.",
+          404
+        );
+      }
+      return category;
     } catch (error) {
-      console.error("Failed to retrieve category.", error);
+      console.error("[Service] - Failed to retrieve category.", error);
       throw error;
     }
   }
 }
-
-export const categoryService = new CategoryService();
