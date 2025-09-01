855c216ff8e0fd2ad54d813f9232a8d916c7df03
diegomaustem <diego.maustem@gmail.com>
2025-09-01 15:19:03 -0300

    :sparkles: feat: Create interface repository category

--- src/interfaces/ICategory.ts
+++
@@ -1,8 +0,0 @@
-import { Decimal } from "../generated/prisma/runtime/library";
-export interface ICategory {
-  id: string;
-  name: string;
-  value: Decimal;
-  creation_date: Date;
-  update_date: Date;
-}
