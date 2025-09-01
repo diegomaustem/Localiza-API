f985682d38495822c10ad64dad3da8b24600547f
diegomaustem <diego.maustem@gmail.com>
2025-09-01 16:19:42 -0300

    :sparkles: feat: Create controller of State

--- src/controllers/StateController.ts
+++ src/controllers/StateController.ts
@@ -1,49 +1,43 @@
 import { Request, Response } from "express";
-import { stateService } from "../services/StateService";
-class StateController {
-  async getStates(req: Request, res: Response): Promise<void> {
+import { IStateService } from "../interfaces/State/IStateService";
+import HttpError from "../errors/HttpError";
+export class StateController {
+  constructor(private readonly service: IStateService) {}
+
+  listStates = async (req: Request, res: Response): Promise<void> => {
     try {
-      const states = await stateService.getStates();
-      res.status(200).json({ code: 200, status: "success", states: states });
+      const states = await this.service.listStates();
+      res.status(200).json({ data: { states: states } });
     } catch (error) {
-      console.error("Error getting states.", error);
+      console.error("[Controller] - Error getting states.", error);
 
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao buscar estados.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error fetching states.",
       });
     }
-  }
+  };
 
-  async getState(req: Request, res: Response): Promise<void> {
-    const stateId: string = req.params.id;
-
+  listState = async (req: Request, res: Response): Promise<void> => {
     try {
-      const state = await stateService.getState(stateId);
-      if (!state) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "Estado não encontrado.",
+      const id = req.params.id;
+      const state = await this.service.listState(id);
+      res.status(200).json({ data: { state: state } });
+    } catch (error) {
+      console.error("[Controller] - Error getting state.", error);
+
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({
+          code: error.code || "INTERNAL_SERVER_ERROR",
+          message: error.message,
         });
         return;
       }
 
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        state: state,
-      });
-    } catch (error) {
-      console.error("Error getting state.", error);
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao buscar estado.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error fetching state.",
       });
     }
-  }
+  };
 }
-
-export const stateController = new StateController();
