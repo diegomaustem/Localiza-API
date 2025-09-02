3716e28fed860b2fa202a09c5529d49e6db40c34
diegomaustem <diego.maustem@gmail.com>
2025-09-01 22:12:25 -0300

    :sparkles: feat: Create service group

--- src/services/GroupService.ts
+++ src/services/GroupService.ts
@@ -1,23 +1,34 @@
-import { IGroup } from "../interfaces/IGroup";
-import { groupRepository } from "../repositories/GroupRepository";
-class GroupService {
-  async getGroups(): Promise<IGroup[]> {
+import HttpError from "../errors/HttpError";
+import { IGroup } from "../interfaces/Group/IGroup";
+import { IGroupRepository } from "../interfaces/Group/IGroupRepository";
+import { IGroupService } from "../interfaces/Group/IGroupService";
+
+export class GroupService implements IGroupService {
+  constructor(private readonly repository: IGroupRepository) {}
+
+  async listGroups(): Promise<IGroup[]> {
     try {
-      return await groupRepository.findMany();
+      return await this.repository.findMany();
     } catch (error) {
-      console.error("Failed to retrieve groups.", error);
+      console.error("[Service] - Failed to retrieve groups.", error);
       throw error;
     }
   }
 
-  async getGroup(groupId: string): Promise<IGroup | null> {
+  async listGroup(id: string): Promise<IGroup | null> {
     try {
-      return await groupRepository.findOne(groupId);
+      const group = await this.repository.findUnique(id);
+      if (!group) {
+        throw new HttpError(
+          "RESOURCE_NOT_FOUND",
+          "Group not found in our records.",
+          404
+        );
+      }
+      return group;
     } catch (error) {
-      console.error("Failed to retrieve group.", error);
+      console.error("[Service] - Failed to retrieve group.", error);
       throw error;
     }
   }
 }
-
-export const groupService = new GroupService();
