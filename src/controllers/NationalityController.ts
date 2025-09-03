7961f418940ac73659f6384a1d9981f04665e5b2
diegomaustem <diego.maustem@gmail.com>
2025-09-03 13:03:52 -0300

    :sparkles: feat: Create controller of nationality

--- src/controllers/NationalityController.ts
+++ src/controllers/NationalityController.ts
@@ -1,179 +1,126 @@
 import { Request, Response } from "express";
-import { nationalityService } from "../services/NationalityService";
-import { INationality } from "../interfaces/INationality";
 import HttpError from "../errors/HttpError";
-class NationalityController {
-  async getNationalities(req: Request, res: Response): Promise<void> {
-    try {
-      const nationalities = await nationalityService.getNationalities();
-      res
-        .status(200)
-        .json({ code: 200, status: "success", nationalities: nationalities });
-    } catch (error) {
-      console.error("Error getting nationalities.", error);
+import { INationalityService } from "../interfaces/Nationality/ICategoryService";
 
+export class NationalityController {
+  constructor(private readonly service: INationalityService) {}
+
+  listNationalities = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const nationalities = await this.service.listNationalities();
+      res.status(200).json({ data: { nationalities: nationalities } });
+    } catch (error) {
+      console.error("[Controller] - Error getting nationalities.", error);
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao buscar nacionalidades.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error while fetching nationalities.",
       });
     }
-  }
+  };
 
-  async getNationality(req: Request, res: Response) {
-    const nationalityId: string = req.params.id;
-
+  listNationality = async (req: Request, res: Response) => {
     try {
-      const nationality = await nationalityService.getNationality(
-        nationalityId
-      );
-      if (nationality === null) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "Nacionalidade não encontrada.",
+      const id = req.params.id;
+      const nationality = await this.service.listNationality(id);
+      res.status(200).json({
+        data: {
+          nationality: nationality,
+        },
+      });
+    } catch (error) {
+      console.error("[Controller] - Error getting nationality.", error);
+
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({
+          code: error.code || "INTERNAL_SERVER_ERROR",
+          message: error.message,
         });
         return;
       }
 
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        nationality: nationality,
-      });
-    } catch (error) {
-      console.error("Error getting nationality.", error);
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao buscar nacionalidade.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error while fetching nationality.",
       });
     }
-  }
+  };
 
-  async createNationality(req: Request, res: Response): Promise<void> {
-    const nationalityData: INationality = req.body;
-
+  create = async (req: Request, res: Response): Promise<void> => {
     try {
-      await nationalityService.nationalityRulesValidation(nationalityData);
-
-      const createdNationality = await nationalityService.createNationality(
-        nationalityData
-      );
-
+      const nationalityData = req.body;
+      const newNationality = await this.service.create(nationalityData);
       res.status(201).json({
-        code: 201,
-        status: "success",
-        message: "Nacionalidade criada com sucesso.",
-        createdNationality: createdNationality,
+        message: "Nationality created successfully.",
+        data: { nationality: newNationality },
       });
     } catch (error) {
+      console.error("[Controller] - Error creating nationality.", error);
       if (error instanceof HttpError) {
         res.status(error.statusCode).json({
-          code: error.statusCode,
-          status: "error",
+          code: error.code || "INTERNAL_SERVER_ERROR",
           message: error.message,
         });
         return;
       }
 
-      console.error("Error creating nationality.", error);
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao criar nacionalidade.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error while creating nationality.",
       });
     }
-  }
+  };
 
-  async updateNationality(req: Request, res: Response): Promise<void> {
-    const nationalityId: string = req.params.id;
-    const nationalityData: INationality = req.body;
-
+  update = async (req: Request, res: Response): Promise<void> => {
     try {
-      const nationality = await nationalityService.getNationality(
-        nationalityId
-      );
-      if (!nationality) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "Nacionalidade não encontrada para atualização.",
-        });
-        return;
-      }
-
-      await nationalityService.nationalityRulesValidation(nationalityData);
-
-      const updatedNationality = await nationalityService.updateNationality(
-        nationalityId,
-        nationalityData
-      );
-
+      const id = req.params.id;
+      const nationalityData = req.body;
+      const updatedNationality = await this.service.update(id, nationalityData);
       res.status(200).json({
-        code: 200,
-        status: "success",
-        message: "Nacionalidade atualizada com sucesso.",
-        updatedNationality: updatedNationality,
+        message: "Nationality updated successfully.",
+        data: { nationality: updatedNationality },
       });
     } catch (error) {
-      console.error("Error updating nationality.", error);
-      res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao atualizar nacionalidade.",
-      });
-    }
-  }
-
-  async deleteNationality(req: Request, res: Response): Promise<void> {
-    const nationalityId: string = req.params.id;
-
-    try {
-      const nationality = await nationalityService.getNationality(
-        nationalityId
-      );
-      if (!nationality) {
-        res.status(404).json({
-          code: 404,
-          status: "error",
-          message: "Nacionalidade não encontrada para exclusão.",
-        });
-        return;
-      }
-
-      await nationalityService.nationalityRulesValidation(
-        undefined,
-        nationalityId
-      );
-
-      const deletedNationality = await nationalityService.deleteNationality(
-        nationalityId
-      );
-
-      res.status(200).json({
-        code: 200,
-        status: "success",
-        message: "Nacionalidade excluída com sucesso.",
-        deletedNationality: deletedNationality,
-      });
-    } catch (error) {
+      console.error("[Controller] - Error updating nationality.", error);
       if (error instanceof HttpError) {
         res.status(error.statusCode).json({
-          code: error.statusCode,
-          status: "error",
+          code: error.code || "INTERNAL_SERVER_ERROR",
           message: error.message,
         });
         return;
       }
+      res.status(500).json({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error while updating nationality.",
+      });
+    }
+  };
+
+  delete = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const id = req.params.id;
+      const deletedNationality = await this.service.delete(id);
+
+      res.status(200).json({
+        message: "Nationality deleted successfully.",
+        data: {
+          nationality: deletedNationality,
+        },
+      });
+    } catch (error) {
       console.error("Error deleting nationality.", error);
+
+      if (error instanceof HttpError) {
+        res.status(error.statusCode).json({
+          code: error.code || "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+        return;
+      }
+
       res.status(500).json({
-        code: 500,
-        status: "error",
-        message: "Erro interno ao excluir nacionalidade.",
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Internal error while deleting nationality.",
       });
     }
-  }
+  };
 }
-
-export const nationalityController = new NationalityController();
